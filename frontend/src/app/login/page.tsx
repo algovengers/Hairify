979b593791773519f47c0866fc6f124434a906cc
Sumit Kumar <sumitkumar@kgpian.iitkgp.ac.in>
2024-05-10 23:57:29 +0530

    made the SignUp and the LoginPage Ui again with a better design

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -56,48 +56,94 @@ function LoginInner() {
     }
   };
   return (
-    <div className="h-screen w-screen flex justify-center items-center bg-teal-400">
-      <div className="flex flex-col items-center gap-4 bg-white p-4 py-8 h-96 justify-center w-80">
-        <div className="text-xl">Hairify</div>
-        <div>Login</div>
+
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="text-xs text-red-900 h-2">{error}</div>
-        <div className="flex flex-col gap-6 items-center w-full">
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => {
-              setError("");
-              setEmail(e.target.value);
-            }}
-            className="bg-zinc-200 p-2 w-full"
-            placeholder="email"
-          />
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => {
-              setError("");
-              setPassword(e.target.value);
-            }}
-            className="bg-zinc-200 p-2 w-full"
-            placeholder="password"
-          />
-          <button
-            className={`bg-black ${
-              loggingIn ? "bg-gray-600" : "bg-black"
-            } text-white p-2 w-2/5`}
-            onClick={handleLogin}
-          >
-            {!loggingIn ? "Login" : "Logging in..."}
-          </button>
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign in to your Hairify account
+          </h2>
         </div>
-        <div className="text-sm">
-          {`Don't have an account ?`}{" "}
-          <span className="text-blue-700 cursor-pointer">
-            <Link href="signup">Signup</Link>
-          </span>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" action="#" method="POST">
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  placeholder="email"
+                  value={email}
+                  onChange={(e) => {
+                    setError("");
+                    setEmail(e.target.value);
+                  }}
+                  required
+                  className="block px-3 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                  Password
+                </label>
+                <div className="text-sm">
+                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  placeholder="password"
+                  value={password}
+                  onChange={(e) => {
+                    setError("");
+                    setPassword(e.target.value);
+                  }}
+                  className="block px-3 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                onClick={handleLogin}
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                {!loggingIn ? "Login" : "Logging in..."}
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Don't have an account?{' '}
+            <Link href="signup" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+              SignUp
+            </Link>
+          </p>
         </div>
       </div>
-    </div>
+    </>
+
+
   );
 }
+
+
+
+
